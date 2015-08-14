923e427bfbdb58ab2651a6e618867db77f7fc409
Marlen Rotert <marlen.rotert@gmail.com>
2015-08-14 16:30:25 +0200

    hbase query with filter

--- UFO_PROJECT/src/main/java/org/fhmuenster/bde/repository/CitiesCountiesRepository.java
+++ UFO_PROJECT/src/main/java/org/fhmuenster/bde/repository/CitiesCountiesRepository.java
@@ -5,6 +5,10 @@ import java.util.List;
 import javax.inject.Inject;
 
 import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.filter.BinaryComparator;
+import org.apache.hadoop.hbase.filter.CompareFilter.CompareOp;
+import org.apache.hadoop.hbase.filter.SingleColumnValueFilter;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.fhmuenster.bde.entity.CitiesCounties;
 import org.springframework.data.hadoop.hbase.HbaseTemplate;
@@ -25,6 +29,11 @@ public class CitiesCountiesRepository {
 	private byte[] qCity = Bytes.toBytes("city");
 	private byte[] qCounty = Bytes.toBytes("county");
 
+	/**
+	 * Liefert alle CitiesCounties aus HBase.
+	 * 
+	 * @return Liste aller CitiesCounties
+	 */
 	public List<CitiesCounties> findAll() {
 		return hbaseTemplate.find(tableName, "cc",
 				new RowMapper<CitiesCounties>() {
@@ -37,5 +46,29 @@ public class CitiesCountiesRepository {
 					}
 				});
 
+	}
+
+	/**
+	 * Beispiel um auf eine bestimmte county zu filtern.
+	 * 
+	 * @param county
+	 * @return Liste der gefundenen CitiesCounties
+	 */
+	public List<CitiesCounties> findFilterByCounty(String county) {
+		return hbaseTemplate.find(
+				tableName,
+				new Scan(Bytes.toBytes("0"), new SingleColumnValueFilter(Bytes
+						.toBytes("cc"), Bytes.toBytes("county"),
+						CompareOp.EQUAL, new BinaryComparator(Bytes
+								.toBytes(county)))),
+				new RowMapper<CitiesCounties>() {
+					@Override
+					public CitiesCounties mapRow(Result result, int rowNum)
+							throws Exception {
+						return new CitiesCounties(Bytes.toString(result
+								.getValue(CF_CC, qCity)), Bytes.toString(result
+								.getValue(CF_CC, qCounty)));
+					}
+				});
 	}
 }
